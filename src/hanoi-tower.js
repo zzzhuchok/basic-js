9a9fc5fcff89ea09ce4c25ac7d6ef3d75efe581f
Rostislav Zhuk <zzzhuchok@gmail.com>
2023-12-13 14:40:38 +0300

    feat: add the second part of the task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,24 +1,34 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Calculate turns number and time (in seconds) required
  * to solve puzzle
- * 
+ *
  * @param {Number} disks number of disks
  * @param {Number} turnsSpeed speed (in turns/hour)
  * @return {Object} object with props turns (number of turns)
  * and seconds (time in seconds)
  *
  * @example
- * 
+ *
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
+function calculateHanoi(disksNumber, turnsSpeed) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let obj = {
+    turns: 0,
+    seconds: 0,
+  };
+
+  obj.turns = Math.pow(2, disksNumber) - 1;
+  obj.seconds = Math.floor((obj.turns * 3600) / turnsSpeed);
+
+  return obj;
 }
 
 module.exports = {
-  calculateHanoi
+  calculateHanoi,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,42 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const season = {
+    spring: [2, 3, 4],
+    summer: [5, 6, 7],
+    fall: [8, 9, 10],
+    winter: [0, 1, 11],
+  };
+
+  if (!date) return "Unable to determine the time of year!";
+  if (!(date instanceof Date)) throw new Error("Invalid date!");
+
+  try {
+        date.toLocaleDateString();
+    } catch (error) {
+        throw new Error('Invalid date!');
+    }
+
+  const monthNum = date.getMonth();
+
+  for (let month in season) {
+    if (season[month].includes(monthNum)) return month;
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
